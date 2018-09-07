6040e801af297c92d15b20b19919d2be0cc11d3b
George G. Vega Yon <g.vegayon@gmail.com>
2018-09-07 11:52:13 -0700

    Prettier plots

--- R/methods.r
+++ R/methods.r
@@ -29,7 +29,34 @@ plot.bite_dyad <- function(x, y = NULL, ...) {
   plot.new()
   plot.window(xlim = ran, ylim = c(.5, 2.5))
 
-  lines(y = rep.int(1, length(x[[1]])), x = x[[1]], col="steelblue", type = "b")
-  lines(y = rep.int(2, length(x[[2]])), x = x[[2]], col="tomato", type = "b")
+  xstart <- diff(ran)*.1+ran[1]
+
+
+
+  cols <- c("steelblue", "tomato")
+  for (i in 1:2) {
+    text(x=xstart, y = i + .1, labels = paste("Subject", i))
+
+    for (k in 1:length(x[[i]]))
+      text(
+        x = x[[i]][k],
+        y = i,
+        labels = substitute(t[i]^j, list(i=i, j=k)),
+        pos = 1, offset = (k %% 2)*.75 + 1
+        )
+
+    lines(
+      y = rep.int(i, length(x[[i]])), x = x[[i]], col=cols[i],
+      type = "b",
+      lwd=2
+      )
+  }
+
+
+#
+#
+#   text(x=xstart, y = 2.1, labels = "Subject 2")
+#   lines(y = rep.int(2, length(x[[2]])), x = x[[2]], col="tomato", type = "b",
+#         lwd=2)
 
 }
